dcfa5ed956fa4cd558073c02df5b22c0fb0b1ecd
Josh <jkinlaw@redhat.com>
2019-03-08 09:15:31 -0500

    adds sample endpoint for entitlements

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
 import {Application, Request, Response} from "express";
 
+import { getEntitlements } from "./entitlements/entitlements.controller";
+
 export async function route(app: Application) {
     app.get( "/", ( req: Request, res: Response) => {
         res.send( "lubDub" );
     });
+
+    app.get("/entitlements/:id", getEntitlements);
 }
